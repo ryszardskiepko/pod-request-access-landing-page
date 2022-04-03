b32b3271ba9e5ab7ac8b5e19828f873b4c8c7b38
Ryszard Skiepko <skiepko.ryszard@gmail.com>
2022-04-03 12:19:16 +0200

    JS remove text success and reset errors

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const textError = document.querySelector(".form__error");
 const form = document.querySelector(".form");
 const validation = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 let errors = [];
+
+button.addEventListener("click", (e) => {
+  textError = classList.remove("form__success");
+  errors = [];
+});

19a5a6155dae6eaf6a64f4cbb0fbd62b10ab964c
Ryszard Skiepko <skiepko.ryszard@gmail.com>
2022-04-03 12:12:31 +0200

    JS queryselector variables declared

---
+++ script.js
@@ -0,0 +1,4 @@
+const button = document.querySelector("#button");
+const email = document.querySelector("#email");
+const textError = document.querySelector(".form__error");
+const form = document.querySelector(".form");

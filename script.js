0e19287357602fe56973f3b880f334eb869dfe7e
Ryszard Skiepko <skiepko.ryszard@gmail.com>
2022-04-03 12:14:34 +0200

    JS email validation and error space

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const button = document.querySelector("#button");
 const email = document.querySelector("#email");
 const textError = document.querySelector(".form__error");
 const form = document.querySelector(".form");
+const validation = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+let errors = [];
